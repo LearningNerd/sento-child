eb12a5c973238962b19c3f2959dc2c87ce36312e
Michelle Rodriguez <michelle.nicole922@gmail.com>
2016-10-22 21:01:20 -0700

    Enqueue google fonts

--- functions.php
+++ functions.php
@@ -1,6 +1,15 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'sento_child_enqueue_styles' );
+// Enqueue child theme
 function sento_child_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 }
+add_action( 'wp_enqueue_scripts', 'sento_child_enqueue_styles' );
+
+// Enqueue Google fonts
+function sento_child_add_google_fonts() {
+
+wp_enqueue_style( 'sento_child-fonts', 'https://fonts.googleapis.com/css?family=Fira+Sans|Ubuntu', false ); 
+}
+add_action( 'wp_enqueue_scripts', 'sento_child_add_google_fonts' );
+

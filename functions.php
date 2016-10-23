038ac6f11548176de128c40566b66a02905f017e
Michelle Rodriguez <michelle.nicole922@gmail.com>
2016-10-23 10:48:42 -0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	sass/style.scss
    #	style.css

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 // Enqueue child theme
 function sento_child_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style' ) );
 }
 add_action( 'wp_enqueue_scripts', 'sento_child_enqueue_styles' );
 
